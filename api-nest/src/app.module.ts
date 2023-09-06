27ae35dc323efc7d6f6adc8b9acf5b179fa6d0b3
Renata Maçãs <renata.macas@gmail.com>
2023-09-06 18:03:35 -0300

    update mongoDB + mongoose

--- api-nest/src/app.module.ts
+++ api-nest/src/app.module.ts
@@ -3,10 +3,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 import { JogadoresController } from './jogadores/jogadores.controller';
 import { JogadoresService } from './jogadores/jogadores.service';
-
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://macasrenata:teste123@cluster0.ehw2vfs.mongodb.net/api-nest?retryWrites=true&w=majority', 
+    MongooseModule.forRoot( process.env.MONGO_DB, 
    ), // configuração do mongoDB
     JogadoresModule
   ], // entrada e saida do module (importar outros modules)
